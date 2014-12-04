85a19db82cf3e23061bf7515e150cc0d82ffacd5
NannanWang <nannanwang.xidian@gmail.com>
2014-12-04 20:03:07 +0800

    index

---
+++ Nannan_Wang_Images/counter_code.php
@@ -0,0 +1,18 @@
+<?php
+//Easy Counter code begins
+
+$ec_username = 'nangua'; // <--- your username
+
+if (!$ec_fsock = fsockopen('www.easycounter.com', 80, $errno, $errstr, 2)) {
+  echo '<img src="http://www.easycounter.com/images/error.png">';
+} else {
+  fputs($ec_fsock, "GET /php.counter.php?username=".urlencode($ec_username)." HTTP/1.0\r\n".
+  "Host:www.easycounter.com\r\n\r\n");
+  $ec_buffer = '';
+  while (!feof($ec_fsock)) $ec_buffer .= fgets($ec_fsock, 1024);
+  echo substr($ec_buffer, strpos($ec_buffer, "\n\r\n")+3);
+  fclose($ec_fsock);
+}
+
+//Easy Counter code ends
+?>
